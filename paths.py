b8e9d750835cc4e6a1ee0b479e3e7832807bca18
norumai01 <johnnydinhnguyen2001@gmail.com>
2026-05-29 01:17:12 -0400

    LLM integrated and confirm response from Discord bot. Added additional utilities.

---
+++ paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+"""
+Set the root directory of the project
+"""
+ROOT = Path(__file__).resolve().parent
--- utils/RAG/llm.py
+++ utils/RAG/llm.py
@@ -1,8 +1,9 @@
 import logging
 import os
+import groq
 from dotenv import load_dotenv
-import httpx
-import Groq
+
+from utils.RAG.sys_prompt import readSystemPrompt
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -19,14 +20,52 @@ def llm(user_input: str, context: str, username: str) -> str:
     Returns:
         str: The generated response from the LLM.
     """
-    LLM_KEY = os.getenv("LLM_KEY") or ""
+    LLM_KEY = os.getenv("LLM_KEY")
+    GROQ_MODEL = os.getenv("GROQ_MODEL") or "meta-llama/llama-4-scout-17b-16e-instruct"
+    MAX_TOKENS: int = 400
 
     if LLM_KEY is None or LLM_KEY == "":
         logger.error("LLM_KEY is not set in the environment variables.")
         return ""
-    
-    client = Groq.Client(api_key=LLM_KEY, timeout=30.0, max_retries=2)
+    if GROQ_MODEL is None or GROQ_MODEL == "":
+        logger.error("GROQ_MODEL is not set in the environment variables.")
+        return ""
+    if MAX_TOKENS is None or MAX_TOKENS <= 0:
+        logger.error("MAX_TOKENS is not set in the environment variables or is not a valid positive integer.")
+        return ""
 
-    
+    # System prompt
+    prompt: str | None = readSystemPrompt()
+    if prompt is None:
+        logger.error("System prompt is empty.")
+        return ""
+    #logger.debug(f"System prompt: {prompt}") # Debugging
 
-    return ""
+    # Add relevant context and username to the prompt
+    if username:
+        prompt += f"\n\nYou are speaking with: {username}."
+    if context:
+        prompt += f"\n\nHere is relevant context and in-game dialogues about you:\n{context}"
+
+    client = groq.Client(api_key=LLM_KEY, timeout=30.0, max_retries=3)
+
+    # Generate response
+    try:
+        response = client.chat.completions.create(
+            model=GROQ_MODEL,
+            messages=[{
+                "role": "system",
+                "content": prompt
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }],
+            max_tokens=MAX_TOKENS,
+            temperature=0.85,
+        )
+        return response.choices[0].message.content.strip()
+
+    except Exception as e:
+        logger.error(f"Error generating response: {e}")
+        return ""
